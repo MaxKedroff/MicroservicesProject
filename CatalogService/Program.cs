0b4a86d63dd107ccb394379a7d7ecfda664f3955
kma <kma@objctv.ru>
2025-10-08 09:23:52 +0500

    оформил слой АПИ с необходимыми CRUDами, настроил dependency injection в Program.cs, и настроил docker и docker-compose с PostgresSql

--- CatalogService/Program.cs
+++ CatalogService/Program.cs
@@ -1,19 +1,40 @@
+using CatalogService.CoreLib.Interfaces;
+using CatalogService.DAL.Data;
+using CatalogService.DAL.Repositories;
+using CatalogService.Logic.Interfaces;
+using CatalogService.Logic.Services;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<CatalogDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+        dbContext.Database.Migrate();
+    }
 }
 
 app.UseHttpsRedirection();
