00ffd3d2765e4818960d6b836a3612fd83213698
kma <kma@objctv.ru>
2025-10-09 21:43:49 +0500

    создал полностью слой Domain Onion архитектуры. которая содержит сущности и интерфейсы взаимодействия с БД

---
+++ BasketService/Domain/Entities/Basket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// корзина конкретного пользователя(она одна у каждого юзера, поэтому, считаем ее ID как UserId Для простоты
+    /// </summary>
+    public class Basket
+    {
+        public Guid UserId { get; set; }
+
+        public List<BasketItem> Items { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Amount);
+
+    }
+}
