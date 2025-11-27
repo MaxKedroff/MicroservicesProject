0702e9c587e4c63c24e931a49c8a3c99b168640a
kma <kma@objctv.ru>
2025-11-27 19:24:38 +0500

    написал свой кастомный DistrbutedSemaphore на основе API Redis

---
+++ CustomDistributedSemaphore/DistributedSemaphoreFactory .cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDistributedSemaphore
+{
+    public class DistributedSemaphoreFactory : IDistributedSemaphoreFactory
+    {
+        private readonly IConnectionMultiplexer _redis;
+
+        public DistributedSemaphoreFactory(IConnectionMultiplexer redis)
+        {
+            _redis = redis;
+        }
+
+        public IDistributedSemaphore CreateSemaphore(string name, int maxCount)
+        {
+            return new RedisDistributedSemaphore(_redis.GetDatabase(), name, maxCount);
+        }
+    }
+}
---
+++ CustomDistributedSemaphore/IDistributedSemaphore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDistributedSemaphore
+{
+    public interface IDistributedSemaphore
+    {
+        Task<bool> TryAcquireAsync();
+        Task AcquireAsync(TimeSpan? timeout = null);
+        Task ReleaseAsync();
+        int CurrentCount { get; }
+        string Name { get; }
+    }
+}
