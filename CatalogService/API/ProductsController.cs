55dbc04cd1e263d73f9406fc277b8557896a22cc
kma <kma@objctv.ru>
2025-11-27 20:34:38 +0500

    интегрировал мой Distributed Semaphore в CatalogService

--- CatalogService/API/ProductsController.cs
+++ CatalogService/API/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using CatalogService.CoreLib.DTOs;
 using CatalogService.CoreLib.DTOs.Requests;
 using CatalogService.Logic.Interfaces;
+using CustomDistributedSemaphore;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +12,14 @@ namespace CatalogService.API
     public class ProductsController : ControllerBase
     {
         public readonly IProductService _productService;
+        private readonly IDistributedSemaphoreFactory _semaphoreFactory;
 
-        public ProductsController(IProductService productService)
+
+        public ProductsController(IProductService productService, IDistributedSemaphoreFactory semaphoreFactory)
+
         {
             _productService = productService;
+            _semaphoreFactory = semaphoreFactory;
         }
 
         [HttpGet]
@@ -35,24 +40,59 @@ namespace CatalogService.API
         [HttpPost]
         public async Task<ActionResult<ProductDetailsDto>> CreateProduct(CreateProductRequest request)
         {
-            var product = await _productService.CreateProductAsync(request);
-            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+            var semaphore = _semaphoreFactory.CreateSemaphore("product_creation", 2);
+            try
+            {
+                await semaphore.AcquireAsync();
+                var product = await _productService.CreateProductAsync(request);
+                return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+            }catch (TimeoutException)
+            {
+                return StatusCode(429, new { Message = "Too many concurrent product creations" });
+            }
         }
 
         [HttpPut("{id}")]
         public async Task<ActionResult<ProductDetailsDto>> UpdateProduct(Guid id, UpdateProductRequest request)
         {
-            var product = await _productService.UpdateProductAsync(id, request);
-            if (product == null) return NotFound();
-            return Ok(product);
+            var semaphore = _semaphoreFactory.CreateSemaphore($"product_update_{id}", 1);
+
+            try
+            {
+                await semaphore.AcquireAsync(TimeSpan.FromSeconds(5));
+                var product = await _productService.UpdateProductAsync(id, request);
+                if (product == null) return NotFound();
+                return Ok(product);
+            }catch (TimeoutException)
+            {
+                return StatusCode(409, new
+                {
+                    Message = "Product is currently being updated by another process. Please try again later."
+                });
+            }
+            finally
+            {
+                await semaphore.ReleaseAsync();
+            }
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProduct(Guid id)
         {
-            var result = await _productService.DeleteProductAsync(id);
-            if (!result) return NotFound();
-            return NoContent();
+            var semaphore = _semaphoreFactory.CreateSemaphore($"product_delete_{id}", 1);
+            try
+            {
+                await semaphore.AcquireAsync();
+
+                var result = await _productService.DeleteProductAsync(id);
+                if (!result) return NotFound();
+                return NoContent();
+            }
+            finally
+            {
+                await semaphore.ReleaseAsync();
+            }
+            
         }
     }
 }
